4d37bbbcdf9fb3ac3472cfdf7008418593e07249
dnkFrvgv <carinefrvga@gmail.com>
2024-03-06 12:01:15 -0300

    add feature of list round and competitors of competition

---
+++ SkateCompScoreboard.Application/Competitions/Features/ListCompetitorsOfCompetition.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkateCompScoreboard.Core.Entities;
+using SkateCompScoreboard.Persistence.Data;
+
+namespace SkateCompScoreboard.Application.Competitions.Features
+{
+    public class ListCompetitorsOfCompetition
+    {
+        public class Query : IRequest<List<RoundCompetitor>>
+        {
+            public Guid CompetitionId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<RoundCompetitor>>
+        {
+            private DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public Task<List<RoundCompetitor>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return _context.RoundCompetitors.Where(
+                    x => x.Round.CompetitionId == request.CompetitionId &&
+                    x.Round.RoundOrder == 1)
+                    .Include(x => x.Competitor)
+                    .ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ SkateCompScoreboard.Application/Competitions/Features/ListRoundsOfCompetition.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkateCompScoreboard.Core.Entities;
+using SkateCompScoreboard.Persistence.Data;
+
+namespace SkateCompScoreboard.Application.Competitions.Features
+{
+    public class ListRoundsOfCompetition
+    {
+        public class List
+        {
+            public class Query : IRequest<List<Round>> 
+            {
+                public Guid CompetitionId {  get; set; }
+            }
+
+            public class Handler : IRequestHandler<Query, List<Round>>
+            {
+                private DataContext _context;
+
+                public Handler(DataContext context)
+                {
+                    _context = context;
+                }
+                public async Task<List<Round>> Handle(Query request, CancellationToken cancellationToken)
+                {
+                    return await _context.Rounds.
+                        Where(x=>x.CompetitionId == request.CompetitionId)
+                        .ToListAsync(cancellationToken);
+                }
+            }
+        }
+    }
+}
